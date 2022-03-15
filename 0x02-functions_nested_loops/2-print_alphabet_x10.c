7167a47cb0623d313a0f29e11598bf3d9e1d88c6
SharonMutabonwa <mutaron37@gmail.com>
2022-03-15 23:09:28 +0200

    print alphabets in 10 lines

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -14,11 +14,13 @@ void print_alphabet_x10(void)
 	i = 0;
 	while (i < 10)
 	{
-		for (ch = 'a'; ch <= 'z'; ch++)
+		ch = 'a';
+		while (ch <= 'z')
 		{
 			_putchar(ch);
+			ch++;
 		}
+		_putchar(ch);
+		ch++;
 	}
-	_putchar('\n');
-	i++;
 }
