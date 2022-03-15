8c765f68448fe3c1a93210727b5814044193dfe9
SharonMutabonwa <mutaron37@gmail.com>
2022-03-16 00:01:51 +0200

    ckecking lowercase character

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* int _islower - checks the code of ALX School students.
+* Description: - checks for lowercase character
+* Return: Always 1.
+*/
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
