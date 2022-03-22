50c625bb33050a9cda4742fd2ca0c92baf1f04f4
SharonMutabonwa <mutaron37@gmail.com>
2022-03-22 03:54:59 +0000

    Do not hate the hacker

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -8,16 +8,22 @@
  */
 int main(void)
 {
-	int sum;
-	char c;
+	int n;
+  int a[5];
+  int *p;
 
-	srand(time(NULL));
-	while (sum <= 2645)
-	{
-		c = rand() % 128;
-		sum += c;
-		putchar(c);
-	}
-	putchar(2772 - sum);
-	return (0);
+  a[2] = 1024;
+  p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+  *(p + 5) = 98;
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
