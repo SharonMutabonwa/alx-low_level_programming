107ede66bd8a578f7b85024244bd65ddd5cf3fad
SharonMutabonwa <mutaron37@gmail.com>
2022-03-23 22:38:26 +0000

    Coping a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* *_strncpy - copies a string
+* @dest: destination string
+* @src: string to be copied
+* @n:number of bytes from src
+* Return: pointer to the resulting string
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a;
+
+	while (a < n && src[a] != '\0')
+	{
+		dest[a] = src[a];
+		a++;
+	}
+	while (a < n)
+	{
+		dest[a] = '\0';
+		a++;
+	}
+	return (dest);
+}
