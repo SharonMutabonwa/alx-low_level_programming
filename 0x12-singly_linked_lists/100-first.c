3244972ee6df2d8013c5ec4020a928f47a7e6a08
SharonMutabonwa <mutaron37@gmail.com>
2022-04-21 21:55:58 +0000

    Printing a sentence before the main function is executed.

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -9,5 +9,5 @@ void func(void) __attribute__ ((constructor));
 void func(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
-	printf("bore my house upon my back!\n");
+	printf("I bore my house upon my back!\n");
 }
