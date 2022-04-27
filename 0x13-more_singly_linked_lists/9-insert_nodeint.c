7430baf279c27ddd2a486ac331fe3d944c5216ab
SharonMutabonwa <mutaron37@gmail.com>
2022-04-27 00:02:50 +0000

    Deleting the node at index index of a listint_t linked list.

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -5,7 +5,7 @@
 * @head: pointer to the first node of the list
 * @idx: the index of the list where the new node should be added
 * @n: data of the new node to insert
-* Return: 1 if it succeeded, -1 if it failed
+* Return: the address of the new node, or NULL if it failed
 */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
