3f9f31004556b86c853c6913c71a5e91279e18d5
SharonMutabonwa <mutaron37@gmail.com>
2022-07-08 22:55:34 +0000

    A function that creates a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -20,7 +20,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	ht->size = size;
-	ht->array = calloc(size, sizeof(hash_table_t));
+	ht->array = calloc(size, sizeof(hash_table_t *));
 	if (ht->array == NULL)
 		return (NULL);
 
